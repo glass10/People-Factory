f88325e3170ad29003ce7141781516fb94dfe7c6
Cameron Glass <glass10@purdue.edu>
2017-06-05 17:24:53 -0400

    Restructure index.js file

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ function handleSubmit(ev){
     ev.preventDefault();
     const f = ev.target;
     const heading = document.querySelector('h1');
-    heading.textContent = f.yourName.value + ' and ' + f.friendName.value;
-    console.log(f.yourName.value + ' and ' + f.friendName.value);
+    const yourName = f.yourName.value;
+    const friendName = f.friendName.value;
+    if(yourName !== "" && friendName !== ""){
+        heading.textContent = yourName + ' and ' + friendName;
+        console.log(yourName + ' and ' + friendName);
+    }
+    else if(yourName !== "" && friendName === ""){
+        heading.textContent = yourName;
+        console.log(yourName);
+    }
+    else if(yourName === "" && friendName !== ""){
+        heading.textContent = friendName;
+        console.log(friendName);
+    }
+    else{
+        heading.textContent = "Put in some names!";
+        console.log('No Names Entered');
+    }
 }
 personForm.addEventListener('submit', handleSubmit);
